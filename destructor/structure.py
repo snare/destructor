f2bb9b6036fcf4ecb9eb384e19692dc0d6445975
Richo Healey <richo@psych0tik.net>
2014-01-03 14:35:05 -0800

    Whitespace

--- destructor/structure.py
+++ destructor/structure.py
@@ -34,7 +34,7 @@ class NodeCollector(c_ast.NodeVisitor):
     def __init__(self, cls):
         self.nodes = []
         self.cls = cls
-        
+
     def visit_collect(self, node):
         if type(node) is self.cls:
             self.nodes.append(node)
@@ -164,7 +164,7 @@ class StructureMember(object):
 
     @property
     def size(self):
-        return self._size * self._array_len        
+        return self._size * self._array_len
 
     @property
     def format(self):
@@ -241,7 +241,7 @@ class Structure(object):
             # create a structure set
             self._ss = StructureSet(source=self._source, filename=filename)
             ast = self._ss.ast
-            
+
             # find the structure by name if one was given
             if self._name:
                 decl = self._ss.decl_named(self._name)
@@ -326,7 +326,7 @@ class Structure(object):
                     type_name = self._tr.name_for_type(t)
 
                     # instantiate the member
-                    member = StructureMember(name=node.name, node=node, type_name=type_name, mode=mode, 
+                    member = StructureMember(name=node.name, node=node, type_name=type_name, mode=mode,
                                              endian=self._endian)
 
                 # store the new member
@@ -362,7 +362,7 @@ class Structure(object):
         infile.seek(offset)
         for m in self._members_ord:
             m.read(infile)
-    
+
     def parse(self, data, offset=0):
         for m in self._members_ord:
             m.parse(data, offset)
@@ -376,7 +376,7 @@ class Structure(object):
 
 class StructureSet(object):
     """
-    A set of structures. Hand this class a header file and then retrieve 
+    A set of structures. Hand this class a header file and then retrieve
     Structure objects by name.
     """
     def __init__(self, source=None, filename=None):
@@ -412,6 +412,6 @@ class StructureSet(object):
         return st
 
     def all_structs(self):
-        return [type(decl.name, (Structure,), 
+        return [type(decl.name, (Structure,),
                     {'_decl': decl, '_ast': self.ast, '_name': decl.name, '_ss': self}) for decl in self.decls]
 
